dfea564d1c94e4008769adecba44546245f30dec
K_Arai00 <attckonk@gmail.com>
2021-01-23 15:58:25 +0900

    GameManagerの追加

--- Assets/Scripts/TestPlayerController.cs
+++ Assets/Scripts/TestPlayerController.cs
@@ -7,26 +7,32 @@ public class TestPlayerController : MonoBehaviour
 {
     [Header("X軸方向に加える力")]
     [SerializeField] float MoveX;
-    private Rigidbody2D rb2d;
+    private Rigidbody2D rb2d = null;
+    private SpriteRenderer spriteRenderer = null;  
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     void Update()
     {
-        Move();
-        Jump();
-    }
+        rb2d.velocity = new Vector2(MoveX, rb2d.velocity.y);
 
-    public void Move()
-    {
-        rb2d.velocity = new Vector2(MoveX, rb2d.velocity.y); 
-            
+        Jump();
     }
 
     private void Jump()
     {
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle" && spriteRenderer.enabled == true)
+        {
+            Destroy(gameObject);
+        }
     }
 }
