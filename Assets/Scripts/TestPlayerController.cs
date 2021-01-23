68b490e68f0d956d567f1b45c0b26b3681a02c33
K_Arai00 <attckonk@gmail.com>
2021-01-23 13:05:57 +0900

    テスト用のスクリプト＆Tile＆Playerの追加

---
+++ Assets/Scripts/TestPlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestPlayerController : MonoBehaviour
+{
+    [Header("X軸方向に加える力")]
+    [SerializeField] float MoveX;
+    public Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    public void Move()
+    {
+        transform.position = new Vector3(MoveX, 0.0f);
+        rb2d.AddForce(transform.position);
+    }
+
+    private void Jump()
+    {
+
+    }
+}
