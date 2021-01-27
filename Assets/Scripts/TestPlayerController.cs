cc5a87ddd2dcb1abd6c68b96eab119707b4778e3
K_Arai00 <attckonk@gmail.com>
2021-01-27 20:21:02 +0900

    画面タップ(Input.GetMouseButtonDown(0))のところをボタンでジャンプするように変更した。

--- Assets/Scripts/TestPlayerController.cs
+++ Assets/Scripts/TestPlayerController.cs
@@ -13,36 +13,36 @@ public class TestPlayerController : MonoBehaviour
     public LayerMask groundLayer;
     [Header("接地判定")]
     public GroundCheck groundCheck;
+    [Header("Jumpボタン")]
+    [SerializeField] Button _jumpButton;
 
-    private Rigidbody2D rb2d = null;
+    private Rigidbody2D _rb2d = null;
     private bool isGrounded = false;
       
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        _rb2d = GetComponent<Rigidbody2D>();
+        _jumpButton.onClick.AddListener(OnClick);
     }
 
     private void Update()
     {
-        rb2d.velocity = new Vector2(MoveX, rb2d.velocity.y);
+        _rb2d.velocity = new Vector2(MoveX, _rb2d.velocity.y);
 
         isGrounded = groundCheck.IsGround();
-
-        if (isGrounded && Input.GetMouseButtonDown(0))
-        {
-            Jump();
-        }
     }
 
     private void FixedUpdate()
     {
-        rb2d.velocity = new Vector2(MoveX, rb2d.velocity.y);
+        _rb2d.velocity = new Vector2(MoveX, _rb2d.velocity.y);
     }
 
-    private void Jump() 
+    void OnClick()
     {
-        rb2d.AddForce(Vector2.up * jumpFroce);  //上方向へ力を加える。
-
-        isGrounded = false;
+        if (isGrounded)
+        {
+            _rb2d.AddForce(Vector2.up * jumpFroce);  //上方向へ力を加える。
+            isGrounded = false;
+        }     
     }
 }
