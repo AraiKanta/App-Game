2b8709bc8c21fff54fd6463ba85519fe30148adf
K_Arai00 <attckonk@gmail.com>
2021-01-30 01:24:06 +0900

    スクリプトの加筆

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    
+    [Header("GameOverテキスト")]
+    public Text gameOverText;
+    [Header("Scoreテキスト")]
+    public Text scoreText;
+
     void Start()
     {
-        
+        //シーンを切り替えてもこのゲームオブジェクトを削除しないようにする
+        DontDestroyOnLoad(gameObject);
     }
 
     void Update()
     {
-       
+
+    }
+
+    private void GameOver() 
+    {
+        
     }
 }
