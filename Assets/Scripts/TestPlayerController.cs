b166e60aa9ef1b06df24deb2b8db35bfb9a2ade4
K_Arai00 <attckonk@gmail.com>
2021-01-30 01:29:47 +0900

    ジャンプ機能を消した。(ジャンプさせずにステージ反転だけでステージを攻略させるほうがいいと思った。)

--- Assets/Scripts/TestPlayerController.cs
+++ Assets/Scripts/TestPlayerController.cs
@@ -7,29 +7,19 @@ public class TestPlayerController : MonoBehaviour
 {
     [Header("X軸方向に加える力")]
     [SerializeField] float MoveX;
-    [Header("ジャンプ力")]
-    [SerializeField] float jumpFroce = 0;
-    [Header("地面のレイヤー")]
-    public LayerMask groundLayer;
     [Header("接地判定")]
     public GroundCheck groundCheck;
-    [Header("Jumpボタン")]
-    [SerializeField] Button _jumpButton;
 
     private Rigidbody2D _rb2d = null;
-    private bool isGrounded = false;
       
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
-        _jumpButton.onClick.AddListener(OnClick);
     }
 
     private void Update()
     {
-        //_rb2d.velocity = new Vector2(MoveX, _rb2d.velocity.y);
-
-        isGrounded = groundCheck.IsGround();
+        
     }
 
     private void FixedUpdate()
@@ -37,12 +27,13 @@ public class TestPlayerController : MonoBehaviour
         _rb2d.velocity = new Vector2(MoveX, _rb2d.velocity.y);
     }
 
-    void OnClick()
+    //　DeadTileの当たり判定
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (isGrounded)
+        if (collision.gameObject.tag == "DeadTile")
         {
-            _rb2d.AddForce(Vector2.up * jumpFroce);  //上方向へ力を加える。
-            isGrounded = false;
-        }     
+            Destroy(gameObject);
+            Debug.Log("Player is collision now");
+        }
     }
 }
