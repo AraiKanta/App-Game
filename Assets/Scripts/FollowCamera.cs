1f07d09077cadc60dcabdbfb9fa3707f8be4a787
K_Arai00 <attckonk@gmail.com>
2021-02-02 20:57:03 +0900

    Playerをカメラが追従するスクリプト

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FollowCamera : MonoBehaviour
+{
+    GameObject playerObj;
+    Transform playerTransForm;
+
+    void Start()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        playerTransForm = playerObj.transform;
+    }
+
+    void LateUpdate()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera() 
+    {
+        transform.position = new Vector3(playerTransForm.position.x + 5, transform.position.y, transform.position.z);
+    }
+}
