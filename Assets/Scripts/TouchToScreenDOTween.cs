be0a26c7a78291dfc193bd5fd4f84304a216d754
K_Arai00 <attckonk@gmail.com>
2021-02-02 22:12:00 +0900

    animationを削除してDOTweenに変更

---
+++ Assets/Scripts/TouchToScreenDOTween.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TouchToScreenDOTween : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    [SerializeField] Text ttsText;
+ 
+    void Start()
+    {
+        ttsText = GetComponent<Text>();
+
+        Dofade();
+    }
+
+    private void Dofade() 
+    {
+        ttsText.DOFade(0.0f, this.duration)
+            .SetEase(Ease.Flash)
+            .SetLoops(-1, LoopType.Restart)
+            .Play();
+    }
+}
