7fe8f83e4b2375c344a236ae90007a5c1748e12a
K_Arai00 <akgame36@gmail.com>
2021-02-09 19:23:44 +0900

    テスト用の背景と背景スクロールのスクリプトの追加

---
+++ Assets/Scripts/ScrollBackGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackGround : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float startPos;
+    public float endPos;
+
+    private void Update()
+    {
+        transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+        if (transform.position.x <= endPos) ScrollEnd();
+    }
+
+    void ScrollEnd()
+    {
+        float excess = transform.position.x - endPos;
+        Vector2 resetartPosition = transform.position;
+        resetartPosition.x = startPos + excess;
+        transform.position = resetartPosition;
+
+        SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+    }
+}
