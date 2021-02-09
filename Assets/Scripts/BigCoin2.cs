71104c548bc204da589b605fe3c6565eb7f272b5
K_Arai00 <akgame36@gmail.com>
2021-02-09 18:41:48 +0900

    BigCoinを取得すると、UIで表示するようにした。スクリプト複数使ってるのがなんか納得いかないが、いまはとりあえず。

---
+++ Assets/Scripts/BigCoin2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BigCoin2 : MonoBehaviour
+{
+    GameObject bigCoin;
+    [SerializeField] GameObject[] bigCoinUI;
+    void Start()
+    {
+        bigCoin = GameObject.Find("BigCoin (2)"); 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (var item in bigCoinUI)
+            {
+                item.SetActive(true);
+            }
+            bigCoin.SetActive(false);
+        }
+    }
+}
