e499f7608c44b2eda3f88b4fff55ad2d8e2ceb62
K_Arai00 <akgame36@gmail.com>
2021-02-16 19:24:00 +0900

    とりあえずDOTweenでタイトルのTouchToScreenを動かした

---
+++ Assets/Scripts/TitleSceneDOTween.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TitleSceneDOTween : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    [SerializeField] Image touchToStart;
+ 
+    void Start()
+    {
+        touchToStart = GetComponent<Image>();
+
+        Dofade();
+    }
+    private void Dofade() 
+    {
+        touchToStart.DOFade(0.0f, this.duration)
+            .SetEase(Ease.Flash)
+            .SetLoops(-1, LoopType.Restart)
+            .Play();
+    }
+}
