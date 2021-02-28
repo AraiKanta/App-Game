2f7cb6d7b0a2ef9150175206b2794c84e736d97c
K_Arai00 <akgame36@gmail.com>
2021-02-28 19:34:07 +0900

    スイッチとなるオブジェクトのスクリプト

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    GameObject switchSprite;
+    GameObject crumblingFloor;
+    [Header("非アクティブのSwitchオブジェクトを入れる")]
+    [SerializeField] GameObject[] switchSprites;
+
+    void Start()
+    {
+        switchSprite = GameObject.Find("LeverSwitch_Black");
+        crumblingFloor = GameObject.Find("CrumblingFloor");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (var item in switchSprites)
+            {
+                item.SetActive(true);
+                Destroy(crumblingFloor);
+            }
+            switchSprite.SetActive(false);
+        }
+    }
+}
