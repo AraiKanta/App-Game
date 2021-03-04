c782e5ebe51fbf87a0f666c9758d6413d9858930
K_Arai00 <akgame36@gmail.com>
2021-03-04 17:02:47 +0900

    書き途中。うまくいってない。

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] private ObstacleSwitch obstacleSwitch;
+    [SerializeField] private float posY = 1f;
+    bool isFall = false;
+
+    public bool IsFall()
+    {
+        obstacleSwitch.IsSwitch();
+
+        Move();
+        return isFall;
+    }
+
+    private void Move()
+    {
+        isFall = true;
+        if (isFall)
+        {
+            this.gameObject.transform.Translate(0, posY * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleSwitch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>障害物のレバースイッチのアクティブ非アクティブさせるやつ</summary>
+public class ObstacleSwitch : MonoBehaviour
+{
+    [SerializeField] private Obstacle obstacle;
+    /// <summary>黒のレバースイッチ</summary>
+    private GameObject _switchSpriteBlack;
+    /// <summary>白のレバースイッチ</summary>
+    private GameObject _switchSpriteWhite;
+    /// <summary>ボックスオブジェクト</summary>
+    private GameObject _obstacle;
+    [Header("非アクティブのSwitchオブジェクトを入れる")]
+    [SerializeField] private GameObject[] _switchSprites;
+
+    bool isSwitch = false;
+
+    void Start()
+    {
+        //_switchSpriteBlack = GameObject.Find("LeverSwitch_Black");
+        _switchSpriteWhite = GameObject.Find("LeverSwitch_White");
+        _obstacle = GameObject.Find("Obstacle");
+    }
+
+    public bool IsSwitch() 
+    {
+        obstacle.IsFall();
+
+        return isSwitch;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {  
+        if (collision.gameObject.tag == "Player")
+        {
+            isSwitch = true;
+            if (isSwitch)
+            {
+                foreach (var item in _switchSprites)
+                {
+                    item.SetActive(true);
+                }
+                //_switchSpriteBlack.SetActive(false);
+                _switchSpriteWhite.SetActive(false);
+            }  
+        }
+    }
+}
