4ed791e572051dbd61e775831acdde04de795f2e
K_Arai00 <akgame36@gmail.com>
2021-03-11 02:42:27 +0900

    ガイド用スプライトの追加

---
+++ Assets/Scripts/Item/Guide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guide : MonoBehaviour
+{
+    private GameObject _Guide;
+    [SerializeField] private GameObject[] _GuideObj;
+    void Start()
+    {
+        _Guide = GameObject.Find("GuideCollider"); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (var item in _GuideObj)
+            {
+                item.SetActive(true);
+            }
+            _Guide.SetActive(false);
+        }
+    }
+}
