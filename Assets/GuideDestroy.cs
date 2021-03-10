fd7f4d3fbbad3819ecd55339d202ae6349398313
K_Arai00 <akgame36@gmail.com>
2021-03-11 02:39:18 +0900

    スクリプトの追加

---
+++ Assets/GuideDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideDestroy : MonoBehaviour
+{
+    [SerializeField] private GameObject _gurideBlack;
+    [SerializeField] private GameObject _gurideWhite;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(_gurideBlack);
+            Destroy(_gurideWhite);
+        }
+    }
+}
