f04e4e3e8d62b83d7753f26379fb5d547c5df0fc
K_Arai00 <akgame36@gmail.com>
2021-03-11 02:42:46 +0900

    ガイドのDOTweenスクリプト

---
+++ Assets/Scripts/UI/GuideDOTween.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GuideDOTween : MonoBehaviour
+{
+    [SerializeField] private float duration;
+    [SerializeField] Image _guide;
+ 
+    void Start()
+    {
+        _guide = GetComponent<Image>();
+
+        var sequence = DOTween.Sequence();
+        sequence.Append(_guide.DOFade(0.0f, this.duration).SetEase(Ease.InOutFlash))
+                .SetLoops(-1, LoopType.Yoyo)
+                .Play();
+    }
+}
