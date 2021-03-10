c088f30bc80a83f85d1eab555689e1fbc7f6c1c8
K_Arai00 <akgame36@gmail.com>
2021-03-10 19:39:42 +0900

    シングルトンパターンを実装する抽象クラス　の作成

---
+++ Assets/Scripts/Manager Relation/SingletonMonoBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+/// <summary>
+/// シングルトンパターンを実装する抽象クラス
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance 
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Type t = typeof(T);
+                _instance = (T)FindObjectOfType(t);
+                if (_instance == null)
+                {
+                    Debug.LogWarning(t + "をアタッチしているGameObjectはありません");
+                    return null;
+                }
+            }
+            return _instance;  
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        if (this != Instance)
+        {
+            Debug.LogWarning(typeof(T) + "は既に他のGameObjectにアタッチされているため、コンポーネントを破棄しました"
+                + "アタッチされているGameObjectは" + Instance.gameObject.name + "です");
+            Destroy(gameObject);
+            return;
+        }
+        //DontDestroyOnLoad(gameObject);は継承先で書く
+    }
+}
