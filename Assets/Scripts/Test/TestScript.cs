17e1213cfd9c1e8ed66a4bdb490cb210463f1d8a
K_Arai00 <akgame36@gmail.com>
2021-03-16 18:30:41 +0900

    追加

---
+++ Assets/Scripts/Test/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] private string num = "GameManager";  
+
+    void Start()
+    {
+        var gameObj = GameObject.Find(num);
+
+        if (gameObj == null)
+        {
+            Instantiate(Resources.Load($"Manager/{num}"),gameObject.transform.parent);
+        }
+    }
+}
