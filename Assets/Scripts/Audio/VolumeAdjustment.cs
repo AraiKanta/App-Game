aedcdfd9a1bf38e73a5753cb9e2f5236b18d2398
K_Arai00 <akgame36@gmail.com>
2021-03-18 00:19:39 +0900

    スクリプトの追加

---
+++ Assets/Scripts/Audio/VolumeAdjustment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeAdjustment: MonoBehaviour
+{
+
+    AudioSource _AudioSource;
+
+    public Slider _Slider;
+
+    private bool isPlay;
+    public bool isSliderChange;
+    void Start()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+        isPlay = true;
+    }
+
+    void Update()
+    {
+        _AudioSource.volume = _Slider.GetComponent<Slider>().normalizedValue;
+    }
+}
