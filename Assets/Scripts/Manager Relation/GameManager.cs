7289e4608fdf664fa6a0bcd91585c742496a54da
K_Arai00 <akgame36@gmail.com>
2021-03-18 00:14:39 +0900

    変更や削除

--- Assets/Scripts/Manager Relation/GameManager.cs
+++ Assets/Scripts/Manager Relation/GameManager.cs
@@ -5,32 +5,16 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(MoveSceneManager))]
-//[RequireComponent(typeof(SaveManager))]
+[RequireComponent(typeof(SaveManager))]
 public class GameManager : SingletonMonoBehaviour<GameManager>
 {
     [Header("PlayerController")]
     public PlayerController _playerController;
-    [Header("パネル")]
-    [SerializeField] private GameObject _gameOverPanel;
-    [Header("ゲームオーバーのテキスト")]
-    [SerializeField] private GameObject _gameOverText;
-    [Header("タイトルに戻るボタン")]
-    [SerializeField] private GameObject _returnToTitle;
-    [Header("リトライボタン")]
-    [SerializeField] private GameObject _retry;
-
     MoveSceneManager _moveSceneManager;
-    //SaveManager saveManager;
+    SaveManager _saveManager;
 
-    //これこのゲームにはいらんかも
-    //ステート
-    //enum State 
-    //{   
-    //    Ready,
-    //    Play,
-    //    GameOvar,
-    //}
-    //State state;
+    bool isGameScene = false;
+    bool oneUse = true;
 
     public void Awake()
     {
@@ -43,88 +27,53 @@ public class GameManager : SingletonMonoBehaviour<GameManager>
         DontDestroyOnLoad(gameObject);
 
         _moveSceneManager = GetComponent<MoveSceneManager>();
-        //saveManager = GetComponent<SaveManager>();
+        _saveManager = GetComponent<SaveManager>();
     }
 
     void Start()
     {
-        //Ready();  
+        _playerController = GetComponent<PlayerController>();
     }
 
-    //void LateUpdate()
-    //{
-    //switch (state) 
-    //{
-    //case State.Ready:
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "TestStage")
+        {
+            isGameScene = true;
+        }
+        else
+        {
+            isGameScene = false;
+            oneUse = true;
 
-    //    break;
-
-    //case State.Play:
-    //    if (_playerController.IsDead()) GameOver();
-    //    break;
-
-    //case State.GameOvar:
-    //    //if (_playerController.IsDead()) GameOver();
-    //    break;
-    //}
-    //}
-
-    //始まる前
-    //private void Ready()
-    //{
-    //    state = State.Ready;
-    //    _playerController.SetSteerActive(false);
-    //}
-
-    //Start
-    //private void GameStart()
-    //{
-    //    state = State.Play;
-    //    _playerController.SetSteerActive(true);
-    //}
+            _playerController = null;
+        }
+        if (isGameScene && oneUse)
+        {
+            oneUse = false;
+            _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        }
+    }
 
     //GameOver
-
     public void LoadComponents() 
     {
         _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
-    public void GameOver() 
-    {
-        //state = State.GameOvar;
 
-        // ポーズUIのアクティブ、非アクティブを切り替え
-        _gameOverPanel.SetActive(!_gameOverPanel.activeSelf);
-        _gameOverText.SetActive(!_gameOverText.activeSelf);
-        _returnToTitle.SetActive(!_returnToTitle.activeSelf);
-        _retry.SetActive(!_retry.activeSelf);
+    public void GameClear() 
+    {
         
-        // ポーズUIが表示されているときは停止
-        if (_gameOverPanel.activeSelf && _gameOverText.activeSelf && _returnToTitle && _retry)
-        {
-            Time.timeScale = 0f;
-        }
-        // ポーズUIが表示されていないときは通常通り進行
-        else
-        {
-            Time.timeScale = 1f;
-        }
+    }
+
+    public void GameOver()
+    {
+        
     }
 
     public void Reload()
     {
         string currentSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(currentSceneName);
-    }
-
-    //リトライ
-    public void RetryClick() 
-    {
-        if (Time.timeScale == 0f)
-        {
-            Time.timeScale = 1f;
-        }
-
-        SceneManager.LoadScene("TestStage");
     }
 }
