a3c7a8a622e3c610c850030d37256f674ccb8065
K_Arai00 <akgame36@gmail.com>
2021-03-18 00:19:26 +0900

    スクリプトの追加

---
+++ Assets/Scripts/Audio/DontDestroyAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//シーン遷移してもオーディオ消さない奴
+public class DontDestroyAudio : SingletonMonoBehaviour<DontDestroyAudio>
+{
+    [SerializeField] private string sceneName;
+
+    public void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this);
+    }
+}
